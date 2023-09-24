d566c0f1d50949a10d9ef2b903f99d382150b701
Chris <118864776+darthchris623@users.noreply.github.com>
2023-09-24 18:32:46 -0400

    Update Movies App (jQuery).js
    Changed the FOR loops to FOR OF loops.

--- Movies App (jQuery).js
+++ Movies App (jQuery).js
@@ -1,5 +1,4 @@
 console.log('Movies App (jQuery)');
-
 const movieTable = $('#movie-list-body'); // For the purpose of event delegation
 const movieInput = $('#movie-title'); // Text input for movie title
 const movieRating = $('#movie-rating'); // Number input for movie rating
@@ -15,24 +14,25 @@ function addMovie(movie, rating) {
     originalArray.push(keyValuePair);
     movieRatingArray.push(keyValuePair);
 };
-
+// Event delegation for delete buttons
 $('tbody').on('click', function (event) {
     if (event.target.tagName === 'BUTTON') {
         event.target.parentElement.parentElement.remove(); // Removes movie from the DOM
-        for (let i = 0; i < movieRatingArray.length; i++) { // Removes movie from the movie rating array
-            if (movieRatingArray[i].movie ===
+        // Removes movie from the movieRatingArray
+        for (let movie of movieRatingArray){
+            if (movie.movie ===
                 event.target.parentElement.previousSibling.previousSibling.innerText) {
-                const index = movieRatingArray.indexOf(movieRatingArray[i]);
+                const index = movieRatingArray.indexOf(movie);
                 if (index > -1) {
                     movieRatingArray.splice(index, 1);
                 }
             }
         };
-        // Removes movie from the original array.
-        for (let i = 0; i < originalArray.length; i++) { // Removes movie from the movie rating array
-            if (originalArray[i].movie ===
+        // Removes movie from the originalArray.
+        for (let movie of originalArray){
+            if (movie.movie ===
                 event.target.parentElement.previousSibling.previousSibling.innerText) {
-                const index = originalArray.indexOf(originalArray[i]);
+                const index = originalArray.indexOf(movie);
                 if (index > -1) {
                     originalArray.splice(index, 1);
                 }
@@ -43,7 +43,6 @@ $('tbody').on('click', function (event) {
 // Submit button with jQuery
 $('#submit-button').on('click', function (event) {
     event.preventDefault();
-    // console.log('You have entere a movie.');
     const movie = movieInput.val();
     const rating = parseFloat($(movieRating).val()); // Rating is a number value, not string
     if (movie.length < 2) { // checks to make sure input value is at least 2 characters long
@@ -75,8 +74,8 @@ $('#name-sort').click(function () {
         return 0;
     });
     // Appends the rating list to the DOM.
-    for (let i = 0; i < newSortedArray.length; i++){
-        $('tbody').append(`<tr><td>${newSortedArray[i].movie}</td><td>${newSortedArray[i].rating}</td><td><button>Delete</button></td><tr>`);
+    for (let movie of newSortedArray){
+        $('tbody').append(`<tr><td>${movie.movie}</td><td>${movie.rating}</td><td><button>Delete</button></td><tr>`);
     };
 });
 // Sorts the movies based on rating from lowest to highest.
@@ -94,8 +93,8 @@ $('#low-to-high').click(function () {
         return 0;
     });
     // Appends the new rating list to the DOM.
-    for (let i = 0; i < newSortedArray.length; i++){
-        $('tbody').append(`<tr><td>${newSortedArray[i].movie}</td><td>${newSortedArray[i].rating}</td><td><button>Delete</button></td><tr>`);
+    for (let movie of newSortedArray){
+        $('tbody').append(`<tr><td>${movie.movie}</td><td>${movie.rating}</td><td><button>Delete</button></td><tr>`);
     };
 });
 // Sorts the movies based on rating from highest to lowest.
@@ -113,8 +112,8 @@ $('#high-to-low').click(function () {
         return 0;
     });
     // Appends the rating list to the DOM.
-    for (let i = 0; i < newSortedArray.length; i++){
-        $('tbody').append(`<tr><td>${newSortedArray[i].movie}</td><td>${newSortedArray[i].rating}</td><td><button>Delete</button></td><tr>`);
+    for (let movie of newSortedArray){
+        $('tbody').append(`<tr><td>${movie.movie}</td><td>${movie.rating}</td><td><button>Delete</button></td><tr>`);
     };
 });
 // Button restores the original order in which the movies were added.
@@ -122,7 +121,7 @@ $('#original-order').click(function () {
     // Deletes the old list from the DOM by iterating over the <tr> tags
     $('tbody').children().remove();
     // Appends the rating list to the DOM.
-    for (let i = 0; i < originalArray.length; i++){
-        $('tbody').append(`<tr><td>${originalArray[i].movie}</td><td>${originalArray[i].rating}</td><td><button>Delete</button></td><tr>`);
+    for (let movie of originalArray){
+        $('tbody').append(`<tr><td>${movie.movie}</td><td>${movie.rating}</td><td><button>Delete</button></td><tr>`);
     };
 });
